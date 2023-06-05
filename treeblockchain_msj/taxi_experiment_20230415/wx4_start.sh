686a72a44d4fdf16bbd52c0cccf67c14ca727a97
LancerEnk <enkidum@163.com>
2023-06-05 18:02:17 +0800

    add treeblockchain_msj

---
+++ treeblockchain_msj/taxi_experiment_20230415/wx4_start.sh
@@ -0,0 +1,7 @@
+# cd /home/zc/20230203-treechain_expermiment/2childs/geth_w11_1
+cd ./geth_wx4
+
+#init
+#/home/zc/go/src/github.com/ethereum/go-ethereum/build/bin/geth --identity "MyEth" --rpc --rpcport "8081" --rpccorsdomain "*" --datadir gethdata --port "30314" --nodiscover --rpcapi "eth,net,personal,web3,miner" --networkid "wx4" init genesisgtrie.json
+
+/home/zc/go/src/github.com/ethereum/go-ethereum/build/bin/geth --identity "MyEth" --rpc --rpcport "8081" --rpccorsdomain "*" --datadir gethdata --port "30314" --nodiscover --rpcapi "eth,net,personal,web3,miner,admin" --ws --wsaddr='localhost' --wsport "8546" --wsorigins='*' --wsapi 'personal,net,eth,web3,admin' --networkid "wx4" --allow-insecure-unlock --dev.period 1 console
